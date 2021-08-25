db9ba0fa3ecd5f09974f58c7a444bc11fad4b0a8
Keziah Biermann <development@kizzycode.de>
2021-08-25 20:37:00 +0200

    Renamed error types

--- src/error.rs
+++ src/error.rs
@@ -10,29 +10,29 @@ use std::{
 /// Creates a new variant
 #[doc(hidden)]
 #[macro_export] macro_rules! e {
-    ($kind:expr, $($arg:tt)*) => ({ $crate::error::STLError::with_string($kind, format!($($arg)*)) })
+    ($kind:expr, $($arg:tt)*) => ({ $crate::error::ErrorImpl::with_string($kind, format!($($arg)*)) })
 }
 /// Creates a new `Error::Serde` kind
 #[macro_export] macro_rules! eserde {
-    ($($arg:tt)*) => ({ e!($crate::error::STLErrorKind::SerdeError, $($arg)*) });
+    ($($arg:tt)*) => ({ e!($crate::error::ErrorKind::SerdeError, $($arg)*) });
 }
 /// Creates a new `Error::InvalidData` kind
 #[macro_export] macro_rules! einval {
-    ($($arg:tt)*) => ({ e!($crate::error::STLErrorKind::InvalidData, $($arg)*) });
+    ($($arg:tt)*) => ({ e!($crate::error::ErrorKind::InvalidData, $($arg)*) });
 }
 /// Creates a new `Error::TypeError` kind
 #[macro_export] macro_rules! etype {
-    ($($arg:tt)*) => ({ e!($crate::error::STLErrorKind::TypeError, $($arg)*) });
+    ($($arg:tt)*) => ({ e!($crate::error::ErrorKind::TypeError, $($arg)*) });
 }
 /// Creates a new `Error::NoSuchKey` kind
 #[macro_export] macro_rules! enokey {
-    ($($arg:tt)*) => ({ e!($crate::error::STLErrorKind::NoSuchKey, $($arg)*) });
+    ($($arg:tt)*) => ({ e!($crate::error::ErrorKind::NoSuchKey, $($arg)*) });
 }
 
 
 /// A `serde_type_error` error kind
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum STLErrorKind {
+pub enum ErrorKind {
     /// A serde related error
     SerdeError,
     /// Invalid encoded data
@@ -42,7 +42,7 @@ pub enum STLErrorKind {
     /// A specified key does not exist
     NoSuchKey
 }
-impl Display for STLErrorKind {
+impl Display for ErrorKind {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         match self {
             Self::SerdeError => write!(f, "Serde error"),
@@ -52,26 +52,26 @@ impl Display for STLErrorKind {
         }
     }
 }
-impl error::Error for STLErrorKind {
+impl error::Error for ErrorKind {
     /* Nothing to implement */
 }
 
 
 // Define our custom `serde_dynamic_typing` error
-define_error!(STLError);
-impl serde::de::Error for STLError<STLErrorKind> {
+define_error!(ErrorImpl);
+impl serde::de::Error for ErrorImpl<ErrorKind> {
     fn custom<T>(msg: T) -> Self where T: Display {
-        Self::with_string(STLErrorKind::SerdeError, msg.to_string())
+        Self::with_string(ErrorKind::SerdeError, msg.to_string())
     }
 }
-impl serde::ser::Error for STLError<STLErrorKind> {
+impl serde::ser::Error for ErrorImpl<ErrorKind> {
     fn custom<T>(msg: T) -> Self where T: Display {
-        Self::with_string(STLErrorKind::SerdeError, msg.to_string())
+        Self::with_string(ErrorKind::SerdeError, msg.to_string())
     }
 }
 
 
 /// A typealias for results with our error kind as specialization
-pub type Error = STLError<STLErrorKind>;
+pub type Error = ErrorImpl<ErrorKind>;
 /// A typealias for results with our error as error-variant
-pub type Result<T = ()> = result::Result<T, STLError<STLErrorKind>>;
+pub type Result<T = ()> = result::Result<T, ErrorImpl<ErrorKind>>;
